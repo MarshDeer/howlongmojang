0be19d59714aaedeeeb56e6286b558cb72723ba4
MarshDeer <marshdeer97@gmail.com>
2024-09-12 17:58:39 -0300

    actually renders now
    im tired ill continue later

--- js/main.js
+++ js/main.js
@@ -11,54 +11,41 @@ queryData();
 
 function buildPage() {
 	let main = document.getElementById("main");
-	for (var promise in arrayPromises) {
-		let item = document.createElement(
-			"details", {
-				"class": "window",
-				"id": promise.nameShort,
-				"style": `--background: /assets/${promise.nameShort}/bg.jpg`
-			});
+	for (const promise of arrayPromises) {
+		let item = document.createElement("details");
+			item.classList.add("window");
+			item.id = promise.nameShort;
+			item.style.setProperty("--background", `url(/assets/${promise.nameShort}/bg.jpg)`);
 		let summary = document.createElement("summary");
-		let timer = document.createElement(
-			"time", {
-				"datetime": promise.dateStart,
-			});
-		timer.innerHTML = "Too long";
-		let closedSummary = document.createElement(
-			"span", {
-				"class": "hide"
-			});
-		closedSummary.innerHTML = `waiting for <em>${promise.nameFull}</em>`;
-		let openSummary = document.createElement(
-			"span", {
-				"class": "show"
-			});
-		openSummary.innerHTML = "Have passed";
+		let timer = document.createElement("time");
+			timer.setAttribute("datetime", promise.dateStart);
+			timer.innerHTML = "Too long";
+		let closedSummary = document.createElement("span");
+			closedSummary.classList.add("hide");
+			closedSummary.innerHTML = `waiting for <em>${promise.nameFull}</em>`;
+		let openSummary = document.createElement("span");
+			openSummary.classList.add("show");
+			openSummary.innerHTML = "Have passed";
 		let itemContent = document.createElement("section");
-		for (paragraph of promise.textBody) {
+		for (const paragraph of promise.textBody) {
 			let chunk = document.createElement("p");
 			chunk.innerHTML = paragraph;
 			itemContent.append(chunk);
 		};
-		let icon = document.createElement(
-			"img", {
-				"src": `/assets/${promise.nameShort}/icon.gif`,
-				"loading": "lazy",
-				"aria-hidden": true,
-				"alt": ""
-			});
-		let footer = document.createElement("em");
-		footer.innerHTML = "We are still waiting."
+		let icon = document.createElement("img");
+			icon.src = `/assets/${promise.nameShort}/icon.gif`;
+			icon.setAttribute("loading", "lazy");
+			icon.setAttribute("aria-hidden", "true");
+			icon.setAttribute("alt", "");
+		let footer = document.createElement("p");
+			footer.innerHTML = "<em>We are still waiting.</em>"
 		if (promise.isFulfilled == true) {
-			let bouncyText = document.createElement(
-				"em", {
-					"class": "fixed"
-				},
-			);
-			bouncyText.innerHTML = promise.textBouncy;
+			let bouncyText = document.createElement("em")
+				bouncyText.classList.add("fixed");
+				bouncyText.innerHTML = promise.textBouncy;
 			closedSummary.innerHTML = `Until the release of <em>${promise.nameFull}</em>`;
 			openSummary.innerHTML = `Until the release of <em>${promise.nameFull}</em>`;
-			footer.innerHTML = promise.textFooter;
+			footer.innerHTML = `<em>${promise.textFooter}</em>`;
 			summary.append(bouncyText);
 		}
 		summary.append(timer, closedSummary, openSummary);
